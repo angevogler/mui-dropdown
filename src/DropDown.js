6e53527ed2c6092370205e0bd108ae3aac9cee7c
Angela Vogler <angelavogler4@gmail.com>
2018-07-17 13:08:56 -0400

    working on overriding default styles

--- src/DropDown.js
+++ src/DropDown.js
@@ -17,8 +17,8 @@ let styles = {
 }
 
 const DropDown = (props) => {
-  console.log(props)
   const classes = props.classes;
+
   const menu_items = props.options.map((option, i) => {
     return (
       <MenuItem
@@ -28,21 +28,22 @@ const DropDown = (props) => {
         {option.value}
       </MenuItem>
     )
-  })
+  });
 
   let input_style = {
     textAlign: 'left',
     padding: '0 5px',
     border: '1px solid black',
     borderRadius: '3px',
-  }
+  };
 
   return (
     <FormControl className="drop_down_outer" style={{minWidth: '150px', margin: '50px'}}>
       <Select
         value={props.selected_option}
         onChange={(e) => props.selected_option_update(e)}
-        input={<Input className="drop_down_input"
+        input={<Input
+          className="drop_down_input"
           classes={{underline: classes.underline}}
           style={input_style}
         />}
--- src/FloatingLabelDropDown.js
+++ src/FloatingLabelDropDown.js
@@ -4,7 +4,9 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import Input from '@material-ui/core/Input';
 import MenuItem from '@material-ui/core/MenuItem';
+import { withStyles } from '@material-ui/core/styles';
 
+/*
 export default class FloatingLabelDropDown extends Component {
   render() {
     let menu_items = this.props.options.map((option, i) => {
@@ -32,3 +34,56 @@ export default class FloatingLabelDropDown extends Component {
     )
   }
 }
+*/
+
+let styles = {
+  underline: {
+    '&:after': {
+      borderBottom: 'none',
+    },
+    '&:before': {
+      borderBottom: 'none',
+    },
+  }
+}
+
+const FloatingLabelDropDown = (props) => {
+    const classes = props.classes;
+
+    const menu_items = props.options.map((option, i) => {
+      return (
+        <MenuItem
+        key={i}
+        className="drop_down_option"
+        value={option.rowguid}>
+          {option.value}
+        </MenuItem>
+      )
+    });
+
+    let input_style = {
+      textAlign: 'left',
+      padding: '0 5px',
+      border: '1px solid black',
+      borderRadius: '3px',
+    };
+
+    return (
+      <FormControl className="drop_down_outer" style={{minWidth: '150px', margin: '50px'}}>
+        <InputLabel className="drop_down_label">Label</InputLabel>
+        <Select
+          value={props.selected_option}
+          onChange={(e) => props.selected_option_update(e)}
+          input={<Input
+            className="drop_down_input"
+            classes={{underline: classes.underline}}
+            style={input_style}
+          />}
+        >
+          {menu_items}
+        </Select>
+      </FormControl>
+    )
+}
+
+export default withStyles(styles)(FloatingLabelDropDown);
